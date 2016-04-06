92b275c5fceb73b7d586a00ec181c81c82b05b7b
Philipp Resl <resl.philipp@gmail.com>
2016-04-06 18:13:53 +0200

    com

--- assembly_filter.py
+++ assembly_filter.py
@@ -3,9 +3,9 @@ from Bio import SeqIO
 import numpy as np
 from scipy import stats
 
-assembly_file = "/Users/sinnafoch/Dropbox/Philipp/Genomes/00_data/Agyrium_rufum/01_assembly/Agy_ruf_genomeV2.fasta"
-blast_file = "/Users/sinnafoch/Dropbox/Philipp/Genomes/00_data/Agyrium_rufum/07_blast/agy_ruf_makerrun1_blastp_results.txt"
-coverage_file = "/Users/sinnafoch/Dropbox/Philipp/Genomes/05_coverage_analysis/coverage_per_contig_agyrium_v2.txt"
+assembly_file = "/Users/sinnafoch/Dropbox/Philipp/Genomes/00_data/Xylographa_parallela/01_genome/Xyl_par_genomeV1.fasta"
+blast_file = "/Users/sinnafoch/Dropbox/Philipp/Genomes/00_data/Xylographa_parallela/06_blast/xyl_par_makerrun1_blastp_results_no_header.txt"
+coverage_file = "/Users/sinnafoch/Dropbox/Philipp/Genomes/00_data/Xylographa_parallela/01_genome/coverage_per_contig_xylographa_v1.txt"
 
 assembly = open(assembly_file,"r")
 blast_results = open(blast_file,"r")
@@ -49,7 +49,6 @@ cov_list = []
 id_list= []
 
 for contig in contigs:
-	i+=1
 	gc = calc_gc(contig.seq)
 	bact = count_blast(contig.id)
 	cov = get_coverage(contig.id)
@@ -57,7 +56,8 @@ for contig in contigs:
 	bact_list.append(bact)
 	cov_list.append(cov)
 	id_list.append(contig.id)
-	#if i == 100:
+	#i+=1
+	#if i == 101:
 	#	break
 
 ## select bad contigs
@@ -73,6 +73,7 @@ conv_cov = stats.norm.interval(0.95, loc=mean_cov, scale=sigma_cov) #does only w
 gc_variance = 0.08
 
 print "Name\tGCContent\tPro.ofBactHits\tCoverage\tAnnotation"
+good_contigs = []
 for (gc, bact, cov, id) in zip(gc_list, bact_list, cov_list, id_list):
 	append = ""
 	if (cov < conv_cov[0] or cov > conv_cov[1]):
@@ -82,6 +83,9 @@ for (gc, bact, cov, id) in zip(gc_list, bact_list, cov_list, id_list):
 	if (bact >= 0.66):
 		append += "blast "		
 	print id + "\t" + str(gc) + "\t" + str(bact) + "\t" + str(cov) + "\t" + append	
+	if append == "" or append == "coverage " or append=="gc-content " or append=="blast ":
+		good_contigs.append(id)
+	
 
 print "GC List", len(gc_list)
 print "Bact List", len(bact_list)
@@ -91,3 +95,7 @@ print "GC INterval:", conv_gc
 print "Is GC Content normaly distributed:", stats.normaltest(gc_list[1:100])
 print "Mean Cov:", conv_cov
 print "Is Coverage Content normaly distributed:", stats.normaltest(cov_list[1:100])
+print
+print "List of good contigs:"
+for contig in good_contigs:
+	print contig
