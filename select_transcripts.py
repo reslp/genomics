e6f3a6dee8f2d2014affc90c41f495c58de63bf6
Philipp Resl <resl.philipp@gmail.com>
2019-02-07 13:00:22 +0100

    include argparse and python3 to select_transcript

--- select_transcripts.py
+++ select_transcripts.py
@@ -1,56 +1,79 @@
 #!/usr/bin/env python
-#last edit: 24.02.16
+#last edit: 07.02.19
 #GENOMICS
 import sys
 from Bio import SeqIO
+import argparse
 
-Info = """Reduced one FASTA file to the IDS from another eg. to extract MAKER transcripts for proteins of a gene family from the whole genome transcript file.
+parser = argparse.ArgumentParser(description="%(prog) reduces one FASTA file to the IDS from another file eg. to extract MAKER transcripts for proteins of a gene family from the whole genome transcript file.")
 
-usage: select_transcripts.py <sourcefile> <queryfile> <invert|normal> > outfile
+parser.add_argument("-i", dest="idfile", help="File containing sequence IDs")
+parser.add_argument("-f", dest="fastafile", help="FASTA file containing sequences")
+parser.add_argument("-m", dest="mode", help="mode of sequence selection. default = normal", choices=["invert", "normal"], default="normal")
+parser.add_argument("-g", dest="glob", help="Specify if IDs need to be perfect matches are just have to be conatined in the sequence id. default = perfect", choices=["include", "perfect"], default="perfect")
 
-<invert|normal> decides if script should invert selection or not.
-
-"""
-invert = True
-if len(sys.argv) < 4:
-	sys.stderr.write(Info)
-	quit()
-else:
-	Source_name = sys.argv[1]
- 	Query_name = sys.argv[2]
- 	what = sys.argv[3]
+if len(sys.argv)<2:
+		parser.print_help()
+		sys.exit()
+Args = parser.parse_args()
+if Args.fastafile == None or Args.idfile == None:
+	sys.stderr.write("Error: Input file missing.")
+	parser.print_help()
+	sys.exit()
+		
+Source_name = Args.fastafile
+Query_name = Args.idfile
+what = Args.mode
+match = Args.glob
 
 Source = open(Source_name, "r")
 Query = open(Query_name,"r")
 
-queries = list(SeqIO.parse(Query, "fasta"))
+queries = []
+for line in Query:
+	queries.append(line.strip())
+
 sequences = list(SeqIO.parse(Source, "fasta"))
+
 
 i=0
 if what == "invert":
-	sys.stderr.write("Info: Invert is set to TRUE\n")
-	names = list()
+	sys.stderr.write("Info: Matches will be inverted.\n")
 	list = list()
-	for query in queries:
-		names.append(query.id)
-	for sequence in sequences:
-		if (sequence.id not in names):
-			list.append(sequence)
-			i +=1
-			print ">%s" % sequence.id
-			print sequence.seq
+	if match == "perfect":
+		for sequence in sequences:
+			if (sequence.id not in names):
+				list.append(sequence)
+				i +=1
+				print (">%s" % sequence.id)
+				print (sequence.seq)
+	if match == "include":
+		for sequence in sequences:
+			for name in names:
+				write = True
+				if (name in sequence.id):
+					write = False
+			if write:
+				list.append(sequence)
+				i +=1
+				print (">%s" % sequence.id)
+				print (sequence.seq)
 elif what == "normal":
 	sys.stderr.write("Info: Invert is set to FALSE\n")
-	for query in queries:
-		for sequence in sequences:
-			if query.id == sequence.id:
-				print ">%s" % sequence.id
-				print sequence.seq
-				i+=1
-else:
-	sys.stderr.write("Please specify either normal or invert\n")
-	sys.stderr.write(Info)
-	quit()
+	if match == "perfect":
+		for query in queries:
+			for sequence in sequences:
+				if query == sequence.id:
+					print (">%s" % sequence.id)
+					print (sequence.seq)
+					i+=1
+	if match == "include":
+		for query in queries:
+			for sequence in sequences:
+				if query in sequence.id:
+					print (">%s" % sequence.id)
+					print (sequence.seq)
+					i+=1
 
 
 string = "Wrote " + str(i) + " sequences to file\n"
